5811926de659a15c2d65eb1e0e992bd86927724a
이성근 <sk.lee@nigc.co.kr>
2025-09-14 20:44:38 +0900

    버튼 클릭 위치 안맞는부분 수정

--- src/ui/SummonButton.ts
+++ src/ui/SummonButton.ts
@@ -14,8 +14,7 @@ export class SummonButton {
         this.label = this.scene.add.text(0, 0, '소환(100골드)', { color: '#ffffff', fontSize: '24px', fontFamily: 'monospace' }).setOrigin(0.5);
         this.container.add([this.bg, this.label]);
 
-        this.bg.setInteractive(new Phaser.Geom.Rectangle(-160, -36, 320, 72), Phaser.Geom.Rectangle.Contains);
-        this.label.setInteractive();
+        this.bg.setInteractive();
 
         const press = () => this.bg.setFillStyle(0x236923);
         const release = () => this.bg.setFillStyle(0x2a7a2a);
@@ -28,8 +27,5 @@ export class SummonButton {
         this.bg.on('pointerdown', press);
         this.bg.on('pointerup', () => { release(); click(); });
         this.bg.on('pointerout', release);
-        this.label.on('pointerdown', press);
-        this.label.on('pointerup', () => { release(); click(); });
-        this.label.on('pointerout', release);
     }
 }
