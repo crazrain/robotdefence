57254f5e733c3234222b6ed9e1564ffa774ddba8
이성근 <sk.lee@nigc.co.kr>
2025-09-15 16:46:07 +0900

    영웅 20개 초과 소환 오류 버그 수정

--- src/systems/GridManager.ts
+++ src/systems/GridManager.ts
@@ -63,7 +63,7 @@ export class GridManager {
 
     public trySummonHero() {
         if (this.scene.heroes.length >= MAX_HEROES) {
-            this.scene.toast(`영웅은 ${MAX_HEROES}명까지 소환할 수 있습니다`, THEME.danger);
+            this.scene.toast.show(`영웅은 ${MAX_HEROES}명까지 소환할 수 있습니다`, THEME.danger);
             return false;
         }
 
@@ -96,7 +96,7 @@ export class GridManager {
         }
 
         if (!targetCell) {
-            this.scene.toast('배치 가능한 자리가 없습니다', THEME.danger);
+            this.scene.toast.show('배치 가능한 자리가 없습니다', THEME.danger);
             return false;
         }
 
